dc10e71c285a8be0f7455c404541b961779d3eb7
Chitra Karki <cbkarki@miners.utep.edu>
2025-11-19 15:19:14 -0700

    changed .csv to .xlsx due to autosave option in .xlsx in OneDrive

--- R/write_clean_file.R
+++ R/write_clean_file.R
@@ -1,6 +1,6 @@
 write_cleanfile_csv = function(dat) {
     #ask user for input, if they want to write an csv output
-    answer <- readline(prompt = "Write clean file as csv? Y/N: ")
+    answer <- readline(prompt = "Write clean file as xlsx? Y/N: ")
 
     if (toupper(answer) == "Y") {
         cat("\nSelect a directory to save the file.\n")
@@ -8,21 +8,21 @@ write_cleanfile_csv = function(dat) {
 
         if(is.na(dir_path) || dir_path == "") {stop("No directory selected. Please select a directory to save file")}
 
-        filename <- readline(prompt = "Enter the filename (with .csv extensiopn): ")
+        filename <- readline(prompt = "Enter the filename (with .xlsx extensiopn): ")
 
         if(is.na(dir_path) || dir_path == "" || is.na(filename) || filename == "") {stop("No file name entered. Please enter file name!!")}
 
         # full path
         full_path <- file.path(dir_path,filename)
         # save file
-        write.csv(dat, file = full_path, row.names = FALSE)
+        openxlsx::write.xlsx(dat, file = full_path, row.names = FALSE)
         cat("File saved at",full_path,"\n")
         cat("\n")
 
     } else if (toupper(answer) == "N") {
         cat("\n File not saved.\n Writing first 10 records  to console: \n")
         print(dat[10,],n=nrow(dat))
-        cat("\n\nRecommendaton: Write the output as .csv and review if the input
+        cat("\n\nRecommendaton: Write the output as .xlsx and review if the input
             is in cleaned state.\n")
         return(invisible(dat))
     } else {
--- R/write_updates.R
+++ R/write_updates.R
@@ -1,6 +1,6 @@
 write_updates_csv = function(dat) {
-    #ask user for input, if they want to write an csv output
-    answer <- readline(prompt = "Write updates as csv? Y/N: ")
+    #ask user for input, if they want to write an xlsx output
+    answer <- readline(prompt = "Write updates as xlsx? Y/N: ")
 
     if (toupper(answer) == "Y") {
         cat("\nSelect a directory to save the file.\n")
@@ -10,14 +10,14 @@ write_updates_csv = function(dat) {
 
         if(is.na(dir_path) || dir_path == "") {stop("No directory selected. Please select a directory to save file")}
 
-        filename <- readline(prompt = "Enter the filename (with .csv extensiopn): ")
+        filename <- readline(prompt = "Enter the filename (with .xlsx extensiopn): ")
 
         if(is.na(dir_path) || dir_path == "" || is.na(filename) || filename == "") {stop("No file name entered. Please enter file name!!")}
 
         # full path
         full_path <- file.path(dir_path,filename)
         # save file
-        write.csv(dat, file = full_path, row.names = FALSE)
+        openxlsx::write.xlsx(dat, file = full_path, row.names = FALSE)
         cat("File saved at",full_path,"\n")
         cat("\n")
     } else if (toupper(answer) == "N") {
